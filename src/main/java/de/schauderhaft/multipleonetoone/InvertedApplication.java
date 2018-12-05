11930951756362c16747e3d8b03d30d149ba8bb0
Jens Schauder <jschauder@pivotal.io>
2018-12-05 08:19:32 +0100

    initial commit

---
+++ src/main/java/de/schauderhaft/multipleonetoone/InvertedApplication.java
@@ -0,0 +1,12 @@
+package de.schauderhaft.multipleonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvertedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvertedApplication.class, args);
+	}
+}
